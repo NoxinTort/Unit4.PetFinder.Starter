88c4c8565799e96591669250bef52dfbcf999cc1
NoxinTort <nherzog33@gmail.com>
2024-03-12 16:08:54 -0600

    3_12_commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = 8080;
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
+    res.send("Homepage!  Yeah!")
     // serve up the public folder as static index.html file
 
 });
@@ -21,31 +22,36 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
+    res.send([pets]);
+    console.log("All pets: " + res);
 
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
-
+    const owner = pets.owner;
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.send(pet);
+    console.log("get pet by owner: " + res);
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
+    const name = pets.name;
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    const petObj = res.send(pet);
+    console.log("get pet by name: " + petObj);
+    
 });
 
 app.listen(PORT, () => {
